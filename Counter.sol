b0fadac6abc06549eb68c2e4d14142505609f6a5
bigjuice-b <136724404+bigjuice-b@users.noreply.github.com>
2023-11-13 09:12:01 +0900

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.4;
 contract Counter {
     int private count = 0;
     function incrementCounter() public {
-        count += 100;
+        count += 1000;
     }
     function decrementCounter() public {
-        count -= 100;
+        count -= 1000;
     }
     function getCount() public view returns (int) {
         return count;
