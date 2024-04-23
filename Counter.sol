8951d70bce5314908758961db9d182a2fdda9500
bigjuice-b <136724404+bigjuice-b@users.noreply.github.com>
2024-04-24 08:46:43 +0900

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -6,6 +6,9 @@ contract Counter {
     function incrementCounter() public {
         count += 1000;
     }
+    function incrementCounter2() public {
+        count += 2000;
+    }
     function decrementCounter() public {
         count -= 1000;
     }
